78be178d668338edb9794b5a20dd136ee72cb9d6
Jules Dupuis <jules.dupuis@ynov.com>
2025-02-06 15:59:22 +0100

    typeorm add

--- apps/tournament/src/app/app.module.ts
+++ apps/tournament/src/app/app.module.ts
@@ -4,9 +4,22 @@ import { TournamentController } from './controllers/tournament/tournament.contro
 import { TournamentRepositoryService } from './repositories/tournament/tournament-repository.service';
 import { ParticipantRepositoryService } from './repositories/participant/participant-repository.service';
 import { ParticipantController } from './controllers/participant/participant.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'azerty',
+      username: 'postgres',
+      entities: [],
+      database: 'postgres',
+      synchronize: true,
+      logging: true,
+    }),
+  ],
   controllers: [PingController, TournamentController, ParticipantController],
   providers: [TournamentRepositoryService, ParticipantRepositoryService],
 })
